8f74809792328b106dc51e9fae29ac77fb611d25
Anton Kurguzenkov <kurguzenkov@gmail.com>
2018-04-15 19:05:09 +0300

    webpack and so on =)

--- view/app.js
+++ view/app.js
@@ -1,33 +1,44 @@
+const [ hClues, vClues ] = require('../data/bw/house-7x7.json');
+
+const px = value => `${value}px`;
+
+const valueFn = value => value;
+
+const cssValueConverters = {
+  width: px,
+  height: px
+};
+
 function createElement(tagName, options = {}) {
   const element = document.createElement(tagName);
 
   const properties = Object.keys(options)
     .filter(key => key !== 'styles')
-    .reduce((props, key) => ({ ...props, [ key ]: options[key] }), {});
+    .reduce((props, key) => ({
+      ...props,
+      [ key ]: options[key]
+    }), {});
   
   Object.assign(element, properties);
-
+  
   const styles = options.styles || {};
 
-  Object.assign(element.style, styles);
+  const adaptedStyles = Object.keys(styles)
+    .reduce((acc, key) => ({
+      ...acc,
+      [key]: (cssValueConverters[key] || valueFn)(styles[key])
+    }), {})
+
+  Object.assign(element.style, adaptedStyles);
 
   return element;
 }
 
 const createDiv = options => createElement('div', options);
 
-function buildField(colCount, rowCount, cellSize) {
-  const width = `${colCount * cellSize}px`;
-  const height = `${rowCount * cellSize}px`;
-
-  const innerCellSize = `${cellSize - 1}px`;
-
-  const field = createDiv({
-    className: 'field',
-    styles: {
-      width, height
-    }
-  });
+function appendCells(field, colCount, rowCount, cellSize) {
+  const width = colCount * cellSize;
+  const height = rowCount * cellSize;
 
   for (let i = 0; i < rowCount; i++) {
     for (let j = 0; j < colCount; j++) {
@@ -47,8 +58,8 @@ function buildField(colCount, rowCount, cellSize) {
 
       const cell = createDiv({
         styles: {
-          width: `${cellSize - (isLastCol ? 0 : 1)}px`,
-          height: `${cellSize - (isLastRow ? 0 : 1)}px`
+          width: cellSize - (isLastCol ? 0 : 1),
+          height: cellSize - (isLastRow ? 0 : 1)
         },
         className: classes.join(' ')
       });
@@ -56,12 +67,73 @@ function buildField(colCount, rowCount, cellSize) {
       field.appendChild(cell);
     }
   }
-  
-  return field;
+}
+
+const findLongestClueLength = clues => (
+  Math.max(...clues.map(clue => clue.length))
+);
+
+function buildNonogram(hClues, vClues, cellSize) {
+  const vCluesHeight = findLongestClueLength(vClues) * cellSize;
+  const hCluesWidth = findLongestClueLength(hClues) * cellSize;
+
+  const fieldWidth = vClues.length * cellSize;
+  const fieldHeight = hClues.length * cellSize;
+
+  const nonogramWidth = fieldWidth + hCluesWidth + 1;
+  const nonogramHeight = fieldHeight + vCluesHeight + 1;
+
+  const nonogram = createDiv({
+    className: 'nonogram',
+    styles: {
+      width: nonogramWidth,
+      height: nonogramHeight
+    }
+  });
+
+  const corner = createDiv({
+    className: 'corner',
+    styles: {
+      width: hCluesWidth,
+      height: vCluesHeight
+    }
+  }); 
+
+  const vCluesContainer = createDiv({
+    className: 'v-clues',
+    styles: {
+      width: fieldWidth,
+      height: vCluesHeight
+    }
+  });
+
+  const hCluesContainer = createDiv({
+    className: 'h-clues',
+    styles: {
+      width: hCluesWidth,
+      height: fieldHeight
+    }
+  });
+
+  const field = createDiv({
+    className: 'field',
+    styles: {
+      width: fieldWidth,
+      height: fieldHeight
+    }
+  });
+
+  appendCells(field, vClues.length, hClues.length, cellSize);
+
+  nonogram.appendChild(corner);
+  nonogram.appendChild(vCluesContainer);
+  nonogram.appendChild(hCluesContainer);
+  nonogram.appendChild(field);
+
+  return nonogram;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const field = buildField(20, 15, 20);
-
-  document.body.appendChild(field);
+  const nonogram = buildNonogram(hClues, vClues, 20);
+  document.body.appendChild(nonogram);
 });
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './view/app.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'view'),
+    filename: 'bundle.js'
+  }
+};
