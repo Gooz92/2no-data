5866a6d0c37492a7b96e9f3312a74a22f58c4ba8
Gooz <kurguzenkov@gmail.com>
2020-03-15 18:56:45 +0300

    add boat nono

--- view/app.js
+++ view/app.js
@@ -1,7 +1,7 @@
 import { createDiv, omit } from './utils.js';
 const solve = require('../solve.js');
 
-const [ hClues, vClues ] = require('../data/bw/duck-10x6.json');
+const [ hClues, vClues ] = require('../data/bw/10x10/boat.json');
 
 function appendCells(field, colCount, rowCount, cellSize, getOptions = () => ({})) {
 
