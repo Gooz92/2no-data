03f329aba077fbd257f0f703ed22e0e4442cd7b7
Gooz <kurguzenkov@gmail.com>
2020-05-21 14:00:59 +0300

    refresh serialization, write test script

--- serialization/index.js
+++ serialization/index.js
@@ -1,4 +1,23 @@
-const nono = require('../data/bw/5x5/dog.json');
-const { nibbles2r64 } = require('../nibbles/nibbles.js');
+const nono = require('../data/bw/15x15/turtle.json');
+const toNibbles = require('./to-nibbles.js');
+const fromNibbles = require('./from-nibbles.js');
+const { nibbles2r64, r64ToNibbles } = require('../nibbles/nibbles.js');
 
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'.split('');
+
+const nibbles = toNibbles(nono);
+
+console.log(JSON.stringify([ nono[0], nono[1] ]).length);
+
+const c = [ nono[0], nono[1] ].map(dim => (
+  dim.map(line => line.join(',')).join(';')
+)).join('/');
+
+console.log(c.length)
+
+console.log(nibbles.length);
+
+const r64data = nibbles2r64(nibbles);
+const restoredNibbles = r64ToNibbles(r64data)
+
+console.log(fromNibbles(restoredNibbles));
